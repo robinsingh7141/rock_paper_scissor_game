f7cd3f0b2d5a92d8100c88161722a4e74649c7b4
robinsingh7141 <68529373+robinsingh7141@users.noreply.github.com>
2023-05-18 01:04:03 -0700

    made a rock paper scissor game
    it is a basic rock paper scissor game i created in python using if else

---
+++ rock_paper_game.py
@@ -0,0 +1,31 @@
+rock_paper_game
+
+import random
+
+options = ("rock","paper", "scissors")
+running = True
+while running:
+    player = None
+    computer = random.choice(options)
+
+    while player not in options:
+        player = input("Enter a choice (rock, paper, scissors): ")
+
+    print(f"Player: {player}")
+    print(f"computer: {computer}")
+
+    if player == computer:
+        print("It's a tie")
+    elif player == "rock" and computer == "scissors":
+        print("you Win!")
+    elif player == "paper" and computer == "rock":
+        print("you Win!")
+    elif player == "scissors" and computer =="paper":
+        print("you Win!")
+    else:
+        print("you lose!")
+
+    if not input("play again ? (y/n): ").lower() == "y":
+        running = False
+        
+print("Thanks for playing!")
